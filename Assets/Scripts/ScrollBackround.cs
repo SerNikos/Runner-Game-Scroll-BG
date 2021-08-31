41630accc080dedbe549809d8111f6a69b82a894
SerNikos <78097143+SerNikos@users.noreply.github.com>
2021-08-31 21:28:51 +0300

    i uploaded the unity files from my pc

---
+++ Assets/Scripts/ScrollBackround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackround : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public int xVelocity, yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+        material.mainTextureOffset += offset * Time.deltaTime; 
+    }
+}
